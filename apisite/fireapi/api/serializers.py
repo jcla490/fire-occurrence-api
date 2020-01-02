280957680a64e6c57e3442b017bbebd1ee3baf22
jcla490 <josh.clark@dnr.wa.gov>
2020-01-02 10:10:08 -0800

    closes #8

--- apisite/fireapi/api/serializers.py
+++ apisite/fireapi/api/serializers.py
@@ -6,7 +6,7 @@ from django_filters.rest_framework import DjangoFilterBackend
 class FiresSerializer(serializers.ModelSerializer):
     class Meta:
         model = Fires
-        fields = ['id', 'country', 'country_iso', 'name', 'size_ac', 'size_ha','start_date', 'end_date', 'cause', 'state', 'state_iso','latitude', 'longitude', 'fire_year', 'fire_month', 'fire_doy']
+        fields = ['id', 'country', 'country_iso', 'state', 'state_iso', 'latitude', 'longitude', 'start_date', 'end_date', 'fire_year', 'fire_month', 'fire_doy', 'name', 'size_ac', 'size_ha', 'cause']
         
         extra_kwargs = {
             'size_ac': {'max_digits': 16, 'decimal_places': 6},
